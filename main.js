81a4dba766eb88566a6399d4fdc70073e1d87f9b
Peter Pan <petarzivkovic85@gmail.com>
2021-06-02 17:07:54 -0700

    added submenu

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const electron = require('electron');
 const url = require('url');
 const path = require('path');
+// const { Menu } = require('electron');
 
-const {app, BrowserWindow} = electron;
+const {app, BrowserWindow, Menu} = electron;
 
 let mainWindow;
 
@@ -11,4 +12,38 @@ app.on('ready', function(){
     // create new window
     mainWindow = new BrowserWindow({});
     // load html into window
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'mainWindow.html'),
+        protocol:'file:',
+        slashes: true
+    }));
+
+    // build menu from template
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+
+    // insert menu
+    Menu.setApplicationMenu(mainMenu);
 });
+
+// create menu template
+const mainMenuTemplate = [
+    {
+        label:'File',
+        submenu:[
+            {
+                label:'Add Item'
+            },
+            {
+                label:'Clear Items'
+            },
+            {
+                label:'Quit',
+                accelerator: process.platform == 'darwin' ? 'Command+Q' :
+                'Ctrl+Q',
+                click(){
+                    app.quit();
+                }
+            }
+        ]
+    }
+];
